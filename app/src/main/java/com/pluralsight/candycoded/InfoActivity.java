57544f3bff59598d8eabdd015629165d368295fc
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2020-10-09 01:43:20 +0200

    🚑 fix(task_2): Ensure local tests pass
    Set the package in a new line

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -27,8 +27,8 @@ public class InfoActivity extends AppCompatActivity {
 
     public void createMapIntent(View view) {
         Uri storeUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-        Intent mapIntent = new Intent(Intent.ACTION_VIEW, storeUri)
-                .setPackage("com.google.android.apps.maps");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, storeUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
         if (mapIntent.resolveActivity(getPackageManager()) != null) {
             startActivity(mapIntent);
         }
